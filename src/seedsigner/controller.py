498e80e88640f4bd1c200763cadc4d635dd4c09a
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-02-24 14:41:02 -0400

    VERSION updated to reflect non-release

--- src/seedsigner/controller.py
+++ src/seedsigner/controller.py
@@ -95,7 +95,7 @@ class Controller(Singleton):
         rather than at the top in order avoid circular imports.
     """
 
-    VERSION = "0.7.0"
+    VERSION = "7 +MultiLanguage"
 
     # Declare class member vars with type hints to enable richer IDE support throughout
     # the code.
